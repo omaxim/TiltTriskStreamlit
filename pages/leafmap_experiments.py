3a85daa2360d57a1cd18f59b33cf89f6849f11e6
Maxim <maxim.oweyssi@gmail.com>
2024-11-06 16:23:34 +0000

    Update leafmap_experiments.py

--- pages/leafmap_experiments.py
+++ pages/leafmap_experiments.py
@@ -88,7 +88,7 @@ else:
         # Handle missing values if necessary
         if nuts_gdf_levelled[weight].isna().any():
             nuts_gdf_levelled[weight] = nuts_gdf_levelled[weight].fillna(0)  # Fill NaNs with 0 or a default value
-        nuts_gdf_levelled[weight] = nuts_gdf_levelled[weight]#.map('{:.2%}'.format)
+        nuts_gdf_levelled[weight] = nuts_gdf_levelled[weight].map('{:.2%}'.format)
         # Initialize a Leafmap object centered on the data points with a closer zoom level
         m2 = leafmap.Map(center=[data_withaddress['latitude'].mean(), data_withaddress['longitude'].mean()])
         # Define the style_function to dynamically apply color based on the `weight` column
@@ -136,7 +136,6 @@ else:
             column=weight,
             layer_name=weight,
             add_legend=False,
-            legend_kwds={"fmt": "{:.2%}"},
             edge_color=None,  # Remove boundary outlines
             edge_width=0,     # Ensure edge width is set to zero
             fill_opacity=0.7,  # Adjust fill opacity for better visibility
