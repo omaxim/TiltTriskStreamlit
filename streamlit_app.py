d9fae0a38b1561ca75e9411949e770e826003bd0
Maxim <maxim.oweyssi@gmail.com>
2024-11-06 17:47:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,7 +117,7 @@ else:
         # Define the style_function to dynamically apply color based on the `weight` column
         # Define the colormap manually (from light to dark)
         vmin = data[weight].min()
-        vmax=0.5*data[weight].max()
+        vmax=data[weight].max()
         if vmin<=-0.001:
             colormap = get_colormap(vmin=vmin,vmax=vmax,num_colors=20,invert=True)
         else:
