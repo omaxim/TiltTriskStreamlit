5728c29009134f7fe99e0215cf4b9e4784299469
Maxim <maxim.oweyssi@gmail.com>
2024-11-06 15:26:27 +0000

    Update leafmap_experiments.py

--- pages/leafmap_experiments.py
+++ pages/leafmap_experiments.py
@@ -87,10 +87,9 @@ else:
         # Handle missing values if necessary
         if nuts_gdf_levelled[weight].isna().any():
             nuts_gdf_levelled[weight] = nuts_gdf_levelled[weight].fillna(0)  # Fill NaNs with 0 or a default value
-
+        nuts_gdf_levelled[weight] = (100*nuts_gdf_levelled[weight]).round(2)
         # Initialize a Leafmap object centered on the data points with a closer zoom level
         m2 = leafmap.Map(center=[data_withaddress['latitude'].mean(), data_withaddress['longitude'].mean()])
-
         # Add a choropleth layer based on NUTS boundaries without outlines
         m2.add_data(
             nuts_gdf_levelled,
