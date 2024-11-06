ad12b82ecf14769ff3f1ead94ea82f7a6c28272e
Maxim <maxim.oweyssi@gmail.com>
2024-11-06 17:27:16 +0000

    Update leafmap_experiments.py

--- pages/leafmap_experiments.py
+++ pages/leafmap_experiments.py
@@ -109,7 +109,7 @@ else:
         # Define the style_function to dynamically apply color based on the `weight` column
         # Define the colormap manually (from light to dark)
 
-        colormap = get_colormap(vmin=data[weight].min(),vmax=data[weight].max(),num_colors=20)
+        colormap = get_colormap(vmin=data[weight].min(),vmax=0.1,num_colors=20)
 
         def style_function(feature):
             # Get the value from the `weight` column for the feature
