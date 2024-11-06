2620b90cfe515a4c39e96c7580de5ef132e06c11
Maxim <maxim.oweyssi@gmail.com>
2024-11-06 15:40:55 +0000

    Update leafmap_experiments.py

--- pages/leafmap_experiments.py
+++ pages/leafmap_experiments.py
@@ -18,7 +18,7 @@ st.logo(icon_image='TheiaLogo.svg',image='logo.png',size='large')
 def load_nuts_data():
     gdf = gpd.read_file("NUTS_RG_60M_2024_4326.shp")
     gdf_country = gdf[gdf['CNTR_CODE'].isin(['FR','DE'])].drop(['NUTS_NAME','COAST_TYPE','URBN_TYPE','MOUNT_TYPE'],axis=1)
-    return gdf_country.loc[~gdf_country['NUTS_ID'].isin(['FRY1','FRY2','FRY3'])]
+    return gdf_country.loc[~gdf_country['NUTS_ID'].isin(['FRY1','FRY2','FRY3','FRY4','FRY5'])] #Remove french colonies from the map
 
 nuts_gdf = load_nuts_data()
 st.title("")
