5fa1b3ca2d7ce41a76478a85ee3f7870b179ecd2
Maxim <maxim.oweyssi@gmail.com>
2024-11-06 15:45:57 +0000

    Update leafmap_experiments.py

--- pages/leafmap_experiments.py
+++ pages/leafmap_experiments.py
@@ -94,6 +94,7 @@ else:
         m2.add_data(
             nuts_gdf_levelled,
             column=weight,
+            vmax=0.2,
             cmap="YlOrRd",
             layer_name="PD Shock Intensity by Region",
             legend_kwds={"fmt": "{:.2%}"},
