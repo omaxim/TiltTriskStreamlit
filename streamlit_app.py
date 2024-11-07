977e6210d3c948b230a59ab75abe104ddefa212d
Maxim <maxim.oweyssi@gmail.com>
2024-11-07 13:01:08 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def load_nuts_data():
 nuts_gdf = load_nuts_data()
 st.title("")
 st.title("")
-col1,sepcol,col2,coly = st.columns([5,1,5,2])
+col1,sepcol,col2 = st.columns([5,2,5])
 col1.title("SME T-risk")
 col2.title("")
 
